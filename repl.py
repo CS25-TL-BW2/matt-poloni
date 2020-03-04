b327908d98f6635ed727ac3400846ce0cf08f691
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2020-03-04 09:57:49 -0500

    Latest updates

--- repl.py
+++ repl.py
@@ -12,24 +12,25 @@ player = Player()
 player.cache_player()
 
 # Current actions
-# world = player.current_world
-# coords = player.current_room.get_coords()
+world = player.current_world
+coords = player.current_room.get_coords()
 
 # target = player.shop_room
 # target = player.pirate_room
 # target = player.mining_room
-# target = player.well_room
+target = player.well_room
+# target = (52, 55)
 
-# path = world.bfs_to_targets(coords, {target})
-# player.travel_route(path)
-# print("DONE")
+path = world.bfs_to_targets(coords, {target})
+player.travel_route(path)
+print("DONE")
 
 # world.print_rooms()
 # player.treasure_hunt()
 # path = world.bfs_to_targets({})
 
 # BLOCKCHAIN
-while True:
-    proof = player.proof_of_work()
-    if proof is not None:
-        player.cooldown(lambda: bc_mine(proof))
+# while True:
+#     proof = player.proof_of_work()
+#     if proof is not None:
+#         player.cooldown(lambda: bc_mine(proof))
